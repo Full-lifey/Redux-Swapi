fa5fe45cab3867b0451a5a8ffe4ae1fb5886b5e0
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-06-19 15:10:46 -0600

    forgot to commit often..... Imported everything and setup redux and middleware in base index.js. Then built most of the reducer, then built actions, finished reducer, then wired up the component. I ran into an issue where my mapStateToProps was wrong, I was missing the reducer in the middle. Fixed that and then checked my fetched data in my actions and finished writing my actions.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-  rootReducer
+  rootReducer,
   /* applyMiddleware goes here */
+  composeEnhancers(applyMiddleware(logger, thunk))
 );
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
